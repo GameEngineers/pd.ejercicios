ab573e264b2919a18f1fff730a67e18da661e651
Jorge Garrido <torkanwarrior@gmail.com>
2015-10-09 18:35:13 +0200

    initial ejercicios

---
+++ src/main/java/es/upm/miw/singleton/factory/ReferencesFactory.java
@@ -0,0 +1,15 @@
+package es.upm.miw.singleton.factory;
+
+import java.util.Map;
+
+public class ReferencesFactory {
+    private static Map<String,Integer> referencesFactory;
+    private static int reference;
+    
+    /*private void ReferencesFctory(){
+        referencesFactory = new ReferencesFactory();
+    }
+    
+    public static */
+    
+}
